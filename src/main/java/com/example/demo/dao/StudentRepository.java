2b8de34c047c18b32917ca96c748c8590419031b
Tristan Elliott <>
2021-02-17 17:16:28 -0400

    creating the dao layer and adding the student repository

---
+++ src/main/java/com/example/demo/dao/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.models.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+	
+
+}
--- src/main/java/com/example/demo/services/StudentServices.java
+++ src/main/java/com/example/demo/services/StudentServices.java
@@ -4,23 +4,24 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dao.StudentRepository;
 import com.example.demo.models.Student;
 
 @Service
 public class StudentServices {
+	
+	private final StudentRepository studentRepository;
+	
+	@Autowired
+	public StudentServices(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
+	}
 
 	public List<Student> getStudents(){
-		return List.of(
-				new Student(
-						1L,
-						"Tom",
-						"Tom@tomMail.com",
-						LocalDate.of(2000, Month.JANUARY, 5),
-						21
-						)
-				);
+		return studentRepository.findAll();
 	}
 }
